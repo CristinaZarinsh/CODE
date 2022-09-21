6832d491c0a4fb179a787a971253dd15ff52b7bb
Cristina_Zarinsh <kot-199613@mail.ru>
2022-09-21 23:08:25 +0300

    Добавлен второй пример

---
+++ Homework01/Ex002/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = -9;
+int b = -3;
+
+if (a > b ) 
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+
+if (a < b ) 
+{
+    Console.Write("min = ");
+    Console.WriteLine(a);
+    
+}
+else
+{
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
