30ed4e1ee85a96e2964bb30bba983a7c11533695
Cristina_Zarinsh <kot-199613@mail.ru>
2022-09-26 20:08:04 +0300

    Добавлена Задача 13

--- Homework02/Ex013/Program.cs
+++ Homework02/Ex013/Program.cs
@@ -9,7 +9,7 @@ int number1 = 654;
 int number2 = 78;
 int number3 = 32679;
 
-void FindDigit(int num)
+void FindThirdDigit(int num)
 {
     if(num >= 1000)
     {
@@ -18,19 +18,27 @@ void FindDigit(int num)
             num /= 10;
         }
         int result = num % 10;
-        Console.WriteLine("result ");
+        Console.WriteLine(result);
     }
     else if(num >= 100)
     {
         int result = num % 10;
-        Console.WriteLine("result");
+        Console.WriteLine(result);
     }
     else
     {
-        Console.WriteLine("Трутьей цифры нет");
+        Console.WriteLine("Трeтьей цифры нет");
     }
 }
 
-Console.Write(number1);
-FindDigit(number1);
+Console.WriteLine(number1);
+FindThirdDigit(number1);
+Console.WriteLine();
+
+Console.WriteLine(number2);
+FindThirdDigit(number2);
+Console.WriteLine();
+
+Console.WriteLine(number3);
+FindThirdDigit(number3);
 Console.WriteLine();
