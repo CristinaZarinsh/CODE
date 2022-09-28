6a61f6783b9a86a194bf5d6be1dc8a76c011d45a
Cristina_Zarinsh <kot-199613@mail.ru>
2022-09-29 00:08:56 +0300

    Есть норм пользовательский интерфейс

--- Seminar02/Ex035/Program.cs
+++ Seminar02/Ex035/Program.cs
@@ -1,15 +1,22 @@
 ﻿//Задача 35. Определить, присутствует ли в заданнои массиве некоторое число.
-
+Console.Clear();
 int count = 10;
 int[] array = new int[count];
 int find = new Random().Next(1, 21);
 int index = 0;
 
+Console.Write("Ищем = ");
+Console.WriteLine(find);
+Console.WriteLine("Массив: ");
+
 while (index < count)
 {
-    array[index] = new Random().Next(1, 101);
+    array[index] = new Random().Next(1, 22);
+    Console.Write(array[index] + " ");
     index += 1;
 }
+
+Console.WriteLine();
 
 index = 0;
 
@@ -17,7 +24,7 @@ while(index < count)
 {
     if (array[index] == find)
     {
-        Console.WriteLine("ДА");
+        Console.WriteLine("Такой элемент есть");
     }
     index += 1;
 }
