df5ebb98529de0fbada96ac48a91780e579e3592
Cristina_Zarinsh <kot-199613@mail.ru>
2022-09-28 23:57:06 +0300

    Задача 35  решена

---
+++ Seminar02/Ex035/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 35. Определить, присутствует ли в заданнои массиве некоторое число.
+
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0;
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 101);
+    index += 1;
+}
+
+index = 0;
+
+while(index < count)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("ДА");
+    }
+    index += 1;
+}
+
+Console.WriteLine("END");
